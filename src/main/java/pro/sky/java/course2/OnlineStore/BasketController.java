a80e46d96183d5c90b01bbd6230174968834b9d7
korrova11 <korrova@yandex.ru>
2023-11-14 01:27:15 +0300

    second commit

--- src/main/java/pro/sky/java/course2/OnlineStore/BasketController.java
+++ src/main/java/pro/sky/java/course2/OnlineStore/BasketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RequestMapping("/store/order")
+@RequestMapping("/order")
 
 @RestController
 public class BasketController {
